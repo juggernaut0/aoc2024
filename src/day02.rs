5996985446bea31274a2c339a93c1c57684db291
Tom Warner <tomwamt@gmail.com>
2024-12-01 22:27:13 -0700

    Day 2

--- src/day02.rs
+++ src/day02.rs
@@ -1,11 +1,47 @@
+use aoc::parse_lines_with;
+use std::collections::HashSet;
+
 pub struct Solution;
 
 impl aoc::Solution for Solution {
-    fn solve_1(&self, _input: String) -> String {
-        "Not implemented".to_string()
+    fn solve_1(&self, input: String) -> String {
+        parse_lines_with(&input, parse_line)
+            .filter(|report| is_safe(report))
+            .count()
+            .to_string()
     }
 
-    fn solve_2(&self, _input: String) -> String {
-        "Not implemented".to_string()
+    fn solve_2(&self, input: String) -> String {
+        parse_lines_with(&input, parse_line)
+            .filter(|report| is_safe_2(report))
+            .count()
+            .to_string()
     }
+}
+
+fn parse_line(line: &str) -> Vec<i32> {
+    line.split_whitespace()
+        .map(|n| n.parse().unwrap())
+        .collect()
+}
+
+fn is_safe(report: &[i32]) -> bool {
+    let diffs: HashSet<_> = report.windows(2).map(|w| w[1] - w[0]).collect();
+    let grad_incr = diffs.iter().all(|diff| (1..=3).contains(diff));
+    let grad_decr = diffs.iter().all(|diff| (-3..=-1).contains(diff));
+    grad_incr || grad_decr
+}
+
+fn is_safe_2(report: &[i32]) -> bool {
+    if is_safe(report) {
+        return true;
+    }
+    for i in 0..report.len() {
+        let mut report = report.to_vec();
+        report.remove(i);
+        if is_safe(&report) {
+            return true;
+        }
+    }
+    false
 }
