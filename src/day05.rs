e3eac0e48fa91f3d31488c91b903223005b8ee2f
Tom Warner <tomwamt@gmail.com>
2024-12-05 00:11:52 -0700

    Day 5 undumb

--- src/day05.rs
+++ src/day05.rs
@@ -1,3 +1,5 @@
+use std::collections::HashSet;
+
 pub struct Solution;
 
 impl aoc::Solution for Solution {
@@ -23,7 +25,7 @@ impl aoc::Solution for Solution {
     }
 }
 
-fn parse_input(input: &str) -> (Vec<(i32, i32)>, Vec<Vec<i32>>) {
+fn parse_input(input: &str) -> (HashSet<(i32, i32)>, Vec<Vec<i32>>) {
     let (rules_str, updates_str) = input.split_once("\n\n").unwrap();
     let rules = rules_str
         .lines()
@@ -41,7 +43,7 @@ fn parse_input(input: &str) -> (Vec<(i32, i32)>, Vec<Vec<i32>>) {
     (rules, updates)
 }
 
-fn validate_update(update: &[i32], rules: &[(i32, i32)]) -> bool {
+fn validate_update(update: &[i32], rules: &HashSet<(i32, i32)>) -> bool {
     rules.iter().all(|(a, b)| {
         let ai = update.iter().position(|n| n == a);
         let bi = update.iter().position(|n| n == b);
@@ -56,21 +58,15 @@ fn middle_number(update: &[i32]) -> i32 {
     update[update.len() / 2]
 }
 
-fn reorder(mut update: Vec<i32>, rules: &[(i32, i32)]) -> Vec<i32> {
-    let mut result = Vec::with_capacity(update.len());
-    while !update.is_empty() {
-        // find the number in update such that there is no rule that places that number after another. i.e. the first number
-        let i = update
-            .iter()
-            .position(|n| {
-                rules
-                    .iter()
-                    .filter(|(_, b)| n == b)
-                    .all(|(a, _)| !update.contains(a))
-            })
-            .unwrap(); // assume it exists, or else input is broken
-        result.push(update.remove(i));
-    }
-
-    result
+fn reorder(mut update: Vec<i32>, rules: &HashSet<(i32, i32)>) -> Vec<i32> {
+    update.sort_by(|a, b| {
+        if rules.contains(&(*a, *b)) {
+            std::cmp::Ordering::Less
+        } else if rules.contains(&(*b, *a)) {
+            std::cmp::Ordering::Greater
+        } else {
+            std::cmp::Ordering::Equal
+        }
+    });
+    update
 }
